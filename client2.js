35c7805abe9a9952721002a390564f565d829165
邵国庆 <sgq861256127@163.com>
2014-04-22 11:31:43 +0800

    update client2.js
    move client connect and close function inside the client module.

--- client2.js
+++ client2.js
@@ -13,32 +13,46 @@ client.on('close', function(){
 	console.log("onClosed");
 });
 
+client.on('error', function(){
+	console.log("con err");
+});
+
+client.on('timeout', function(){
+	console.log("socket timed out");
+});
+
 function getData(key){
+	client.connect();
 	client.get(key, function(err, result){
 		if(err) console.log("get error");
 		console.log("result is " + result);
+		connect.close();
 	});
 }
 
 // console.log(getData("hello"));//由于node是异步io的，故在getData中注册了取缓存完成的回调函数后，并没有等取完缓存，而是直接返回 故此处打印出来的是空值。
 
 function addData(key, value){
+	client.connect();
 	client.add(key, value, function(err){
 		if(err){
 			console.log("add error");
 			return;
 		}
 		console.log("add success");
+		client.close();
 	});
 }
 
 function deleteData(key){
+	client.connect();
 	client.delete(key, function(err){
 		if(err){
 			console.log("delete error");
 			return;
 		}
 		console.log("deleted");
+		client.close();
 	});
 }
 
